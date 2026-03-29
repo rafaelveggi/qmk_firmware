cfe016caa3e7dd06baa58917a98561db5ded714a
rafaelveggi <rafaelveggi@gmail.com>
2026-03-29 13:20:32 +0100

    with claudes help

--- keyboards/handwired/rafa64/keymaps/default/keymap.c
+++ keyboards/handwired/rafa64/keymaps/default/keymap.c
@@ -1,11 +1,26 @@
 #include QMK_KEYBOARD_H
 
+enum tap_dance_codes {
+    TD_ESC_GRV,
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_ESC_GRV]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRAVE),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_GRAVE        , KC_1   , KC_2   , KC_3   , KC_4   , KC_5  , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
+    TD(TD_ESC_GRV)  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5  , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
     KC_TAB          , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T  , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_DEL ,
-    LCTL_T(KC_ESC)  , KC_A   , KC_S   , KC_D   , KC_F   , KC_G  , _______, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
-    SC_LSPO         , KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V  , _______, KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, SC_RSPC,
-    KC_0            , KC_LALT, KC_LGUI, _______, _______, KC_SPC, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT
+    MT(MOD_LSFT, KC_HOME), MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_F), KC_G, _______, KC_H, MT(MOD_RSFT, KC_J), MT(MOD_RCTL, KC_K), MT(MOD_LALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT, KC_ENT ,
+    MT(MOD_LSFT, KC_END) , KC_Z, KC_X  , KC_C   , KC_V   , KC_B  , _______, _______, KC_N   , KC_M  ,  KC_COMM, KC_DOT , KC_SLSH, MT(MOD_RSFT, KC_UP),
+    LALT(KC_TAB)    , KC_LGUI, KC_LALT, _______, _______, KC_SPC, _______, _______, _______, _______, LT(1, KC_BSLS), MT(MOD_RALT, KC_LEFT), MT(MOD_RGUI, KC_DOWN), MT(MOD_RCTL, KC_RIGHT)
+  ),
+  [1] = LAYOUT(
+    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5 , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 };
